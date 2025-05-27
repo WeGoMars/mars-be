8bd0904cec954cdc805d69cb33ab70ccb28464d9
kimerum333 <kimerum333@gmail.com>
2025-05-27 20:34:29 +0900

    fix: 포트 변경, 로컬 전용 db 설정 제거

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
   app.use(cookieSession({
     keys: ["mysessionkey"]
   }))
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
