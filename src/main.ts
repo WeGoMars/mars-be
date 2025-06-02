67d5a85e8efb66be408fa69c0ec04009b617c801
kimerum333 <kimerum333@gmail.com>
2025-06-02 18:02:27 +0900

    fix: cors 누락 패치

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,10 @@ const cookieSession = require('cookie-session');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
    app.enableCors({
-    origin: 'http://localhost:3000',  // 프론트엔드 주소
+    origin: [
+      'http://localhost:3000',
+      'http://13.220.145.152:3000'
+    ],  // 프론트엔드 주소
     credentials: true,                // 쿠키, 인증 포함
   });
   
